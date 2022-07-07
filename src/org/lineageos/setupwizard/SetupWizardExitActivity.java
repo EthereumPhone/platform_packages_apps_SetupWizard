71cad1ec2d0d505e3132a53e41365d79630d470f
Markus Haas <haas.markus@protonmail.com>
2022-07-07 18:10:27 +0200

    ethOSV1 changes
    Change-Id: I2a5a4de8db64409c0fffa5732e3fe3407d33b0d2

--- src/org/lineageos/setupwizard/SetupWizardExitActivity.java
+++ src/org/lineageos/setupwizard/SetupWizardExitActivity.java
@@ -25,7 +25,14 @@ import android.annotation.Nullable;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.net.wifi.WifiManager;
+import android.provider.Settings.Secure;
+import android.content.Context;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.security.MessageDigest;
 import org.lineageos.setupwizard.util.PhoneMonitor;
 import org.lineageos.setupwizard.util.SetupWizardUtils;
 
@@ -41,6 +48,41 @@ public class SetupWizardExitActivity extends BaseSetupWizardActivity {
         }
         SetupWizardUtils.enableCaptivePortalDetection(this);
         PhoneMonitor.onSetupFinished();
+        final Context context = this;
+        Runnable run = new Runnable() {
+            public void run() {
+                int responseCode = 0;
+                HttpURLConnection urlConnection = null;
+
+                String android_id = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
+                WifiManager wifiManager = getSystemService(WifiManager.class);
+                String macAddress = wifiManager.getConnectionInfo().getMacAddress();
+                String ethOSID = sha256(android_id + ":" + macAddress);
+
+                String urlString = "https://us-central1-imx-minting-ethos.cloudfunctions.net/submitHash?hash="
+                        + ethOSID;
+
+                System.out.println("SETUPWIZARD_HASH: (Trying to do request)");
+                try {
+                    URL url = new URL(urlString);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    responseCode = urlConnection.getResponseCode();
+                } catch (MalformedURLException malformed) {
+                    System.out.println("SETUPWIZARD_HASH: (MalformedURLException) " + malformed.getMessage());
+                } catch (IOException ioexc) {
+                    System.out.println("SETUPWIZARD_HASH: (IOException) " + ioexc.getMessage());
+                } finally {
+                    if (urlConnection != null) {
+                        System.out.println("SETUPWIZARD_HASH: (Disconnecting now) " + responseCode);
+                        urlConnection.disconnect();
+                    }
+                }
+                System.out.println("SETUPWIZARD_HASH: (Finish Request)");
+            }
+        };
+
+        new Thread(run).start();
+
         launchHome();
         finish();
         applyForwardTransition(TRANSITION_ID_FADE);
@@ -53,6 +95,24 @@ public class SetupWizardExitActivity extends BaseSetupWizardActivity {
         startActivity(new Intent("android.intent.action.MAIN")
                 .addCategory("android.intent.category.HOME")
                 .addFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_CLEAR_TASK));
+        startActivity(getPackageManager().getLaunchIntentForPackage("io.metamask"));
+    }
+
+    public String sha256(final String base) {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
